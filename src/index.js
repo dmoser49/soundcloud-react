c3b49f2fbdec8cdd1f5927f68c88aa70688b8978
Daniel Moser <danielmoser49@gmail.com>
2017-11-16 20:41:19 -0600

    simple render by mapping through array ;

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My Minimal React Webpack Babel Setup';
+const tracks = [
+  {
+    title: 'first track'
+  },
+  {
+    title: 'second track'
+  }
+];
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <div>
+    {tracks.map(track => (
+        <div>{track.title}</div>
+      ))}
+  </div>,
   document.getElementById('app')
 );
 
