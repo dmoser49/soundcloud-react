a2bc7f6c365f136015422d0881ecb151b8629900
Daniel Moser <danielmoser49@gmail.com>
2017-11-16 22:42:28 -0600

    add .gitignore file, remove node_modules from git, add tests

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './stores/configureStore';
+import * as actions from './actions';
 import Stream from './components/Stream';
 
 const tracks = [
   {
-    title: 'first track'
+    title: 'Some track'
   },
   {
-    title: 'second track'
+    title: 'Some other track'
   }
 ];
 
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
+
 ReactDOM.render(
-  <div>
-  <Stream tracks={tracks} />
-  </div>,
+  <Stream />,
   document.getElementById('app')
 );
 
